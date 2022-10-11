1ccff8293f9b85c46cc69f9ec582e69f1dfd26a8
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-11 22:57:23 +0900

    Task_23 solved

---
+++ Task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+string input()
+{
+    Console.Write("Введите число: ");
+    string result = Console.ReadLine();
+    return result;
+}
+int len = int.Parse(input());
+int[] arr = new int[len]; // определили массив длинной len-1
+
+
+for (int i = 1; i < (len+1); i++)
+{
+    arr[i-1] = Convert.ToInt32(Math.Pow(i , 3));
+    Console.Write($"{arr[i-1]}; ");
+}
+
