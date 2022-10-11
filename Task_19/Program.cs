c22f9347ad31c246361d31732de451c9d8d82e89
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-11 22:02:53 +0900

    Task_19 solved

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string input()
+{
+    Console.Write("Введите число/слово: ");
+    string result = Console.ReadLine();
+    return result;
+}
+string text = (input());
+
+char[] strreverse = text.ToCharArray(); // делаем из строки массив, чтобы затем перевернуть его
+Array.Reverse(strreverse); // переворачиваем массив (со строками такого метода нет)
+string textreverse=String.Concat<char>(strreverse); // обратно делаем из массива строку
+
+if (textreverse.ToLower() == text.ToLower()) //добавим нечуствительность к регистру
+   {Console.WriteLine("Да, это палиндром");}
+else
+    {Console.WriteLine("Нет, это не палиндром");}
+
